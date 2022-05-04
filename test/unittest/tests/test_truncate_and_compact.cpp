dc92a686fb93abc32723a4f3ce41a590fe5ad0dc
Jeremy Thorp <jeremy.thorp@mongodb.com>
2022-05-04 15:45:25 +1000

    Improvements after chat with Alex.

--- test/unittest/tests/test_truncate_and_compact.cpp
+++ test/unittest/tests/test_truncate_and_compact.cpp
@@ -8,6 +8,7 @@
 
 #include <catch2/catch.hpp>
 #include <iostream>
+#include <format>
 #include "wt_internal.h"
 #include "wiredtiger.h"
 #include "utils.h"
@@ -69,28 +70,36 @@ TEST_CASE("Truncate and compact: table", "[compact]")
     std::string table_name = "table:access2";
     std::string file_name = "file:access2.wt";
 
+    // maybe these are too small
     std::string config =
       "key_format=S,value_format=S,allocation_size=512b,internal_page_max=512b,leaf_page_max=512b";
     REQUIRE(session->create(session, table_name.c_str(), config.c_str()) == 0);
 
+    REQUIRE(conn.getWtConnection()->set_timestamp(conn.getWtConnection(), "oldest_timestamp=1") == 0);
+    REQUIRE(conn.getWtConnection()->set_timestamp(conn.getWtConnection(), "stable_timestamp=1") == 0);
+
     WT_CURSOR* cursor = nullptr;
     REQUIRE(session->open_cursor(session, table_name.c_str(), nullptr, nullptr, &cursor) == 0);
 
+//    set oldest and stable timestamps
+
     {
-        // Add some key/value pairs, with timestamp 10
-        REQUIRE(session->begin_transaction(session, nullptr) == 0);
-        REQUIRE(session->timestamp_transaction_uint(session, WT_TS_TXN_TYPE_COMMIT, 10) == 0);
-
-        // Add 9000 key/values
-        for (int i = 1000; i < 10000; i++) {
-            std::basic_string key = testcase_key_base + std::to_string(i);
-            std::basic_string value = testcase_value_base + std::to_string(i);
-            cursor->set_key(cursor, key.c_str());
-            cursor->set_value(cursor, value.c_str());
-            REQUIRE(cursor->insert(cursor) == 0);
+        // Add some key/value pairs, with timestamp 0x10
+        int maxOuter = 100;
+        int maxInner = 1000;
+        for (int outer = 0; outer < maxOuter; outer++) {
+            REQUIRE(session->begin_transaction(session, nullptr) == 0);
+            for (int inner = 0; inner < maxInner; inner++) {
+                int index = 1000000 + (outer * maxInner) + inner;
+                std::basic_string key = testcase_key_base + std::to_string(index);
+                std::basic_string value = testcase_value_base + std::to_string(index);
+                cursor->set_key(cursor, key.c_str());
+                cursor->set_value(cursor, value.c_str());
+                REQUIRE(cursor->insert(cursor) == 0);
+            }
+            std::string transactionConfig = std::string("commit_timestamp=10");
+            REQUIRE(session->commit_transaction(session, transactionConfig.data()) == 0); // set ts here.
         }
-
-        REQUIRE(session->commit_transaction(session, nullptr) == 0);
         //REQUIRE(session->checkpoint(session, nullptr) == 0);
     }
 
@@ -113,25 +122,27 @@ TEST_CASE("Truncate and compact: table", "[compact]")
 //    }
 
     {
-        // Truncate, with timestamp = 20
+        // Truncate, with timestamp = 0x30
+        // Need to trigger fast truncate, which will truncate whole pages at one.
+        // Need to fast truncate an internal page as well for this test.
         std::cout << "Truncate" << std::endl;
         REQUIRE(session->begin_transaction(session, nullptr) == 0);
-        REQUIRE(session->timestamp_transaction_uint(session, WT_TS_TXN_TYPE_COMMIT, 20) == 0);
 
         WT_CURSOR* truncate_start = nullptr;
         REQUIRE(session->open_cursor(session, table_name.c_str(), nullptr, nullptr, &truncate_start) == 0);
-        std::string key_start = testcase_key_base + std::to_string(3000);
+        std::string key_start = testcase_key_base + std::to_string(1003000);
         truncate_start->set_key(truncate_start, key_start.c_str());
         REQUIRE(truncate_start->search(truncate_start) == 0);
 
         WT_CURSOR* truncate_end = nullptr;
         REQUIRE(session->open_cursor(session, table_name.c_str(), nullptr, nullptr, &truncate_end) == 0);
-        std::string key_end = testcase_key_base + std::to_string(8999);
+        std::string key_end = testcase_key_base + std::to_string(1008999);
         truncate_end->set_key(truncate_end, key_end.c_str());
         REQUIRE(truncate_end->search(truncate_end) == 0);
         REQUIRE(session->truncate(session, nullptr, truncate_start, truncate_end, nullptr) == 0);
 
-        REQUIRE(session->commit_transaction(session, nullptr) == 0);
+        std::string transactionConfig = std::string("commit_timestamp=30");
+        REQUIRE(session->commit_transaction(session, transactionConfig.data()) == 0); // set ts here.
         //REQUIRE(session->checkpoint(session, nullptr) == 0);
     }
 
@@ -154,9 +165,9 @@ TEST_CASE("Truncate and compact: table", "[compact]")
         REQUIRE(session->compact(session, table_name.c_str(), nullptr) == 0);
     }
     {
-        // Read the key/value pairs, at timestamp 15 (ie before the truncate)
+        // Read the key/value pairs, at timestamp 0x20 (ie before the truncate)
         REQUIRE(session->begin_transaction(session, nullptr) == 0);
-        REQUIRE(session->timestamp_transaction_uint(session, WT_TS_TXN_TYPE_READ, 15) == 0);
+        REQUIRE(session->timestamp_transaction_uint(session, WT_TS_TXN_TYPE_READ, 0x20) == 0);
         char const *key = nullptr;
         char const *value = nullptr;
         REQUIRE(cursor->reset(cursor) == 0);
@@ -175,7 +186,7 @@ TEST_CASE("Truncate and compact: table", "[compact]")
         REQUIRE(ret == WT_NOTFOUND); // Check for end-of-table.
         REQUIRE(session->commit_transaction(session, nullptr) == 0);
         std::cout << "number of key:value pairs: " << numValues << std::endl;
-        REQUIRE(numValues == 9000); // We should see the number of values from prior to the truncate
+        REQUIRE(numValues == 10000); // We should see the number of values from prior to the truncate
     }
 
     {
